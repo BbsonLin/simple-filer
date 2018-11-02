c14f25bdc759c9cc7a4750cf3c7f85381b763045
Bobson Lin <bobson801104@gmail.com>
2018-11-03 00:07:30 +0800

    feat(backend/app.py): Use flask-filer and flask-cors extension / Add /map API to see backend rule ma

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,17 @@
 import os
 
-from flask import Flask, request, render_template
+from flask import Flask, jsonify, current_app, render_template, url_for
+from flask_filer import Filer
+from flask_cors import CORS
 
-TEMPLATE_FOLDER = os.path.abspath('./dist/')
-STATIC_FOLDER = os.path.abspath('./dist/_nuxt/')
-UPLOAD_FOLDER = '/tmp/simple_filer_upload/'
+TEMPLATE_FOLDER = os.path.abspath('../frontend/dist/')
+STATIC_FOLDER = os.path.abspath('../frontend/dist/_nuxt/')
+UPLOAD_FOLDER = '/tmp/'
 
 app = Flask(__name__, template_folder=TEMPLATE_FOLDER, static_folder=STATIC_FOLDER)
+app.config['FILER_ROOT_PATH'] = UPLOAD_FOLDER
+filer = Filer(app)
+cors = CORS(app)
 
 
 @app.route('/')
@@ -14,17 +19,18 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    uploaded_files = request.files.getlist("file")
-    for uploaded_file in uploaded_files:
-        file_path = os.path.join(UPLOAD_FOLDER, uploaded_file.filename)
-        uploaded_file.save(file_path)
+@app.route('/map', methods=['GET'])
+def map():
+    def has_no_empty_params(rule):
+        defaults = rule.defaults if rule.defaults is not None else ()
+        arguments = rule.arguments if rule.arguments is not None else ()
+        return len(defaults) >= len(arguments)
 
-    return 'Upload success'
-
-
-if __name__ == '__main__':
-    if not os.path.exists(UPLOAD_FOLDER):
-        os.mkdir(UPLOAD_FOLDER)
-    app.run(host='0.0.0.0', use_reloader=True, use_debugger=True)
+    links = []
+    for rule in current_app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    return jsonify(data=links)
